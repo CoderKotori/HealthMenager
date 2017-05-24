2a135e3ff65ba1cde72f82ebcdcab5245f01e0f8
unknown <18401644217@163.com>
2017-05-24 19:40:36 +0800

    Healthmenager

---
+++ js/hospital.js
@@ -0,0 +1,9 @@
+    function search() {
+        document.form1.action = "search.jsp";
+        document.form1.submit();
+    }
+
+    function reset() {
+        document.form1.action = "reset.jsp";
+        document.form1.submit();
+    }
