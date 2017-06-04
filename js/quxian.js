8d171ca3c03bddf67123150d3aaf17dd3c031245
unknown <18401644217@163.com>
2017-06-04 15:19:44 +0800

    表格修改

--- js/quxian.js
+++ js/quxian.js
@@ -1,82 +1,221 @@
-var option = {
-    title: {
-        text: '一周内血压变化',
-        subtext: '血压(mmHg)'
-    },
-    tooltip: {
-        trigger: 'axis'
-    },
-    legend: {
-        data: ['收缩压', '舒张压']
-    },
-    toolbox: {
-        show: true,
-        feature: {
-            dataZoom: {
-                yAxisIndex: 'none'
-            },
-            dataView: { readOnly: false },
-            magicType: { type: ['line', 'bar'] },
-            restore: {},
-            saveAsImage: {}
-        }
-    },
-    xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-    },
-    yAxis: {
-        type: 'value',
-        axisLabel: {
-            formatter: '{value} mmHg'
-        }
-    },
-    series: [{
-            name: '收缩压',
-            type: 'line',
-            data: [130, 134, 136, 132, 135, 130, 134],
-            markPoint: {
-                data: [
-                    { type: 'max', name: '最大值' },
-                    { type: 'min', name: '最小值' }
-                ]
-            },
-            markLine: {
-                data: [
-                    { type: 'average', name: '平均值' }
-                ]
+function week() {
+    $("#healthecharts1").show();
+    $("#healthecharts2").hide();
+    $("#healthecharts3").hide();
+    var myChart = echarts.init(document.getElementById('healthecharts1'));
+    var option = {
+        title: {
+            text: '一周内血压变化',
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['收缩压', '舒张压']
+        },
+        toolbox: {
+            show: true,
+            feature: {
+                dataZoom: {
+                    yAxisIndex: 'none'
+                },
+                dataView: { readOnly: false },
+                magicType: { type: ['line', 'bar'] },
+                restore: {},
+                saveAsImage: {}
             }
         },
-        {
-            name: '舒张压',
-            type: 'line',
-            data: [80, 82, 80, 75, 76, 80, 74],
-            markPoint: {
-                data: [
-                    { name: '周最低', value: -2, xAxis: 1, yAxis: -1.5 }
-                ]
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        },
+        yAxis: {
+            type: 'value',
+            scale: 'ture',
+            name: '血压(mmHg)'
+        },
+        series: [{
+                name: '收缩压',
+                type: 'line',
+                data: [130, 134, 136, 132, 135, 130, 134],
+                markPoint: {
+                    data: [
+                        { type: 'max', name: '最大值' },
+                        { type: 'min', name: '最小值' }
+                    ]
+                },
+                markLine: {
+                    data: [
+                        { type: 'average', name: '平均值' }
+                    ]
+                }
             },
-            markLine: {
-                data: [
-                    { type: 'average', name: '平均值' },
-                    [{
-                        symbol: 'none',
-                        x: '90%',
-                        yAxis: 'max'
-                    }, {
-                        symbol: 'circle',
-                        label: {
-                            normal: {
-                                position: 'start',
-                                formatter: '最大值'
-                            }
-                        },
-                        type: 'max',
-                        name: '最高点'
-                    }]
-                ]
+            {
+                name: '舒张压',
+                type: 'line',
+                data: [80, 82, 80, 75, 76, 80, 74],
+                markPoint: {
+                    data: [
+                        { type: 'max', name: '最大值' },
+                        { type: 'min', name: '最小值' }
+                    ]
+                },
+                markLine: {
+                    data: [
+                        { type: 'average', name: '平均值' },
+                    ]
+                }
             }
-        }
-    ]
+        ]
+    }
+    myChart.setOption(option);
+};
+
+function month() {
+    $("#healthecharts1").hide();
+    $("#healthecharts2").show();
+    $("#healthecharts3").hide();
+    var myChart = echarts.init(document.getElementById('healthecharts2'));
+    var option = {
+        title: {
+            text: '一个月内血压变化',
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['收缩压', '舒张压']
+        },
+        toolbox: {
+            show: true,
+            feature: {
+                dataZoom: {
+                    yAxisIndex: 'none'
+                },
+                dataView: { readOnly: false },
+                magicType: { type: ['line', 'bar'] },
+                restore: {},
+                saveAsImage: {}
+            }
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        },
+        yAxis: {
+            type: 'value',
+            scale: 'ture',
+            name: '血压(mmHg)'
+        },
+        series: [{
+                name: '收缩压',
+                type: 'line',
+                data: [130, 134, 136, 132, 135, 130, 134],
+                markPoint: {
+                    data: [
+                        { type: 'max', name: '最大值' },
+                        { type: 'min', name: '最小值' }
+                    ]
+                },
+                markLine: {
+                    data: [
+                        { type: 'average', name: '平均值' }
+                    ]
+                }
+            },
+            {
+                name: '舒张压',
+                type: 'line',
+                data: [80, 82, 80, 75, 76, 80, 74],
+                markPoint: {
+                    data: [
+                        { type: 'max', name: '最大值' },
+                        { type: 'min', name: '最小值' }
+                    ]
+                },
+                markLine: {
+                    data: [
+                        { type: 'average', name: '平均值' },
+                    ]
+                }
+            }
+        ]
+    }
+    myChart.setOption(option);
+};
+
+function year() {
+    $("#healthecharts1").hide();
+    $("#healthecharts2").hide();
+    $("#healthecharts3").show();
+    var myChart = echarts.init(document.getElementById('healthecharts3'));
+    var option = {
+        title: {
+            text: '一年内血压变化',
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['收缩压', '舒张压']
+        },
+        toolbox: {
+            show: true,
+            feature: {
+                dataZoom: {
+                    yAxisIndex: 'none'
+                },
+                dataView: { readOnly: false },
+                magicType: { type: ['line', 'bar'] },
+                restore: {},
+                saveAsImage: {}
+            }
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        },
+        yAxis: {
+            type: 'value',
+            scale: 'ture',
+            name: '血压(mmHg)'
+        },
+        series: [{
+                name: '收缩压',
+                type: 'line',
+                data: [130, 134, 136, 132, 135, 130, 134],
+                markPoint: {
+                    data: [
+                        { type: 'max', name: '最大值' },
+                        { type: 'min', name: '最小值' }
+                    ]
+                },
+                markLine: {
+                    data: [
+                        { type: 'average', name: '平均值' }
+                    ]
+                }
+            },
+            {
+                name: '舒张压',
+                type: 'line',
+                data: [80, 82, 80, 75, 76, 80, 74],
+                markPoint: {
+                    data: [
+                        { type: 'max', name: '最大值' },
+                        { type: 'min', name: '最小值' }
+                    ]
+                },
+                markLine: {
+                    data: [
+                        { type: 'average', name: '平均值' },
+                    ]
+                }
+            }
+        ]
+    }
+    myChart.setOption(option);
 };
