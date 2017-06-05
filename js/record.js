b78da1d3f79140cd177f0f01a3735ac509fa656a
unknown <18401644217@163.com>
2017-06-05 10:16:01 +0800

    健康记录弹窗

--- js/record.js
+++ js/record.js
@@ -1,7 +1,7 @@
  $('button[id=btn_record]').on('click', function() {
      var d = dialog({
          title: '血压记录',
-         content: '舒张压<input/>',
+         content: '<label>舒张压 <input autofocus size="10"/>  mmHg<label><br/><br/><label>收缩压 <input size="10"/>  mmHg<label>',
          okValue: '确 定',
          quickClose: true,
          ok: function() {
@@ -13,7 +13,9 @@
          },
          follow: this,
          cancelValue: '取消',
-         cancel: function() {}
+         cancel: function() {
+             alert('确定关闭？')
+         }
      });
      d.show();
  });
